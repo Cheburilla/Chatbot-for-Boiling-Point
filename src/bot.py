74a550d528138f0334d2494acc486ad61cd86944
Alexander Safronov <sanya.safron2003@gmail.com>
2023-03-11 15:03:28 +0600

    initial commit ♿

---
+++ src/bot.py
@@ -0,0 +1,17 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher.filters import Command
+from config_reader import config
+
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=config.bot_token.get_secret_value())
+# Диспетчер
+dp = Dispatcher()
+
+@dp.message(Command("answer"))
+async def cmd_answer(message: types.Message):
+    await message.answer("Это простой ответ")
