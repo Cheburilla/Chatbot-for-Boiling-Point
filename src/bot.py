f8bfa28c9802a1b83244918985f548c2c93df865
Alexander Safronov <sanya.safron2003@gmail.com>
2023-03-11 16:13:07 +0600

    мапм

--- src/bot.py
+++ src/bot.py
@@ -3,6 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.dispatcher.filters import Command
 from config_reader import config
+import requests
 
 
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -12,6 +13,11 @@ bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="MarkdownV2")
 # Диспетчер
 dp = Dispatcher()
 
-@dp.message(Command("answer"))
-async def cmd_answer(message: types.Message):
-    await message.answer("Это простой ответ")
+url = config.server_url
+
+@dp.message(Command("predict"))
+async def predict(message: types.Message):
+    r = requests.post(url, data={"bot_guid": config.bot_guid, "message": str(message)})
+    answer = r.json()['answer']
+    await message.answer(answer)
+
