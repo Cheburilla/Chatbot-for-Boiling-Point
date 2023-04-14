ce2a612f506595b3922e471e7ec2b60a88ed3171
Людмила <flyver33@gmail.com>
2023-04-14 22:18:09 +0600

    выкуммыв

--- src/manager_handlers/predict_handler.py
+++ src/manager_handlers/predict_handler.py
@@ -7,8 +7,6 @@ from utils.config_reader import config
 def predict(message: types.Message):
     url = config.server_url.get_secret_value()
     payload = dict(bot_guid=config.bot_guid.get_secret_value(), message=str(message.text), client_id=message.from_user.id)
-    print(url, payload)
     r = requests.post(url, json=payload)
-    print(r.json())
     answer = r.json()['answer']
     return message.answer(answer)
